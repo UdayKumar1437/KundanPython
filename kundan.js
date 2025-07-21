d67600d3e72bf39df3ae37eaba945743a466e14d
UdayKumar1437 <udaykumarvalapudasu@gmail.com>
2025-07-21 07:34:46 +0530

    pushed dom

---
+++ kundan.js
@@ -0,0 +1,15 @@
+const element = document.getElementById("name")
+// element.innerHTML = "<p>Kundan</p>"
+const element1 = document.getElementById("uday")
+// element1.style.color = "red"
+// const element = document.getElementsByTagName("h1")
+// const element = document.getElementsByClassName("myname")[0]
+const anc = document.getElementsByTagName("a")[0]
+// Element.style.property = value
+element.style.color = "blue"
+
+element1.setAttribute("class","uday1")
+
+element1.className = "kundan"
+anc.href  = "https://www.udaykumar.tech"
+console.log(element1);
