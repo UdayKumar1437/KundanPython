1c0656c57ee793556c7e9219b0103cf00805f0e0
UdayKumar1437 <udaykumarvalapudasu@gmail.com>
2025-07-28 07:42:49 +0530

    Functions GEC

---
+++ hoisting.js
@@ -0,0 +1,33 @@
+
+
+
+
+// getName();
+// console.log(x);
+
+// var x = 7;
+// console.log(x);
+
+// var getName=()=> {
+//     console.log("Kundan");
+
+// }
+
+
+
+var x = 1
+a()
+b()
+console.log(x);
+
+function a() {
+    var x = 10;
+    console.log(x);
+
+}
+
+function b() {
+    var x = 100;
+    console.log(x);
+
+}
