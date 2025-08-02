1e829af79c979d526b3486c9aaa0d122a4c0f26b
UdayKumar1437 <udaykumarvalapudasu@gmail.com>
2025-08-02 07:29:37 +0530

    Block Scope and Shadowing

--- hoisting.js
+++ hoisting.js
@@ -15,19 +15,100 @@
 
 
 
-var x = 1
-a()
-b()
-console.log(x);
+// var x = 1
+// a()
+// b()
+// console.log(x);
 
-function a() {
-    var x = 10;
-    console.log(x);
+// function a() {
+//     var x = 10;
+//     console.log(x);
 
+// }
+
+// function b() {
+//     var x = 100;
+//     console.log(x);
+
+// }
+
+
+// console.log(a);
+// var a =10 ;
+
+// if (a===undefined)
+// {
+//     console.log("a is undefined");
+    
+// }
+// else{
+//     console.log("a is not defined");
+    
+// }
+
+
+// var b =100
+// function a()
+// {
+//     var b = 10;
+//     c()
+//     function c()
+//     {
+//         // console.log(a);
+        
+//         // var b = 1000
+//         d()
+//         function d()
+//         {
+//             console.log(b);
+//         }
+//         console.log(b);
+//     }   
+// }
+// a()
+
+// console.log(b);
+// Temporal Dead Zone
+// let a = 10
+// const b = 200
+// console.log(a);
+
+
+
+// var b = 200
+
+
+// {
+//     const a = 10
+//     var b = 100
+//     console.log(a);
+    
+// }
+// console.log(b);
+
+// if(true)
+// {
+//     console.log("Kundanika");
+//     console.log("Uday");
+    
+// }
+
+let b = 100
+{
+    var a = 10
+    let b = 20
+    const c = 30
+    console.log(a);
+    console.log(b);
+    console.log(c);
 }
 
-function b() {
-    var x = 100;
-    console.log(x);
-
+console.log(b);
+// console.log(b);
+// console.log(c);
+for(let i = 0;i<=10;i++)
+{
+    console.log(i);
+    
 }
+
